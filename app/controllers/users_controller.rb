bcf5c3c7b188d6979d8ce32455dcaacdbd6444c6
Tanner Brunscheon <tanner-brunscheon@uiowa.edu>
2017-10-19 16:48:02 +0000

    Did work on login and register

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+    def new
+    # default: render 'new' template
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
   root 'welcome#index'
+  match '/login', to: 'sessions#new', via: :get
+  match '/login_create', to: 'sessions#create', via: :post
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
