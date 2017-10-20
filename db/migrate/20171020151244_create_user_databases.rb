34bd2eac8329ad759db89b927bd780292835e428
Alexander Michel <alexander-michel@uiowa.edu>
2017-10-20 15:17:09 +0000

    Remade user database, working on testing

--- db/migrate/20171020151244_create_user_databases.rb
+++ db/migrate/20171020151244_create_user_databases.rb
@@ -1,10 +1,11 @@
-class CreateUsers < ActiveRecord::Migration[5.1]
+class CreateUserDatabases < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t|
+    create_table :user_databases do |t|
       t.string :username, :null => false, unique: true
       t.string :email, :null => false, unique: true
       t.string :displayName
       t.string :password
+      t.timestamps
     end
   end
 end
--- spec/databases/users_database_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'spec_helper'
-require 'rails_helper'
-
-describe 
