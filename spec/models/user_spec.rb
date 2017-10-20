e1742f8c1f3134c8b92104f209675c8c013b4eb5
Alic Szecsei <aszecsei@gmail.com>
2017-10-20 20:50:27 +0000

    Add rspec tests for user model validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,24 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  it "has a valid factory"
-  it "is invalid without a firstname"
-  it "is invalid without a lastname"
-  it "returns a contact's full name as a string"
+  it "has a valid factory" do
+    FactoryGirl.create(:user).should be_valid
+  end
+  it "is invalid without a username" do
+    FactoryGirl.build(:user, username: nil).should_not be_valid
+  end
+  it "is invalid without an email" do
+    FactoryGirl.build(:user, email: nil).should_not be_valid
+  end
+  it "is invalid without a display name" do
+    FactoryGirl.build(:user, displayName: nil).should_not be_valid
+  end
+  it "does not allow duplicate usernames" do
+    FactoryGirl.create(:user, username: 'test')
+    FactoryGirl.build(:user, username: 'test').should_not be_valid
+  end
+  it "does not allow duplicate emails" do
+    FactoryGirl.create(:user, email: 'test@test.com')
+    FactoryGirl.build(:user, email: 'test@test.com').should_not be_valid
+  end
 end
