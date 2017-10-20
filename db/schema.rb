20fd186fb4e945c0956012f92ee564eb1f779105
Alexander Michel <alexander-michel@uiowa.edu>
2017-10-20 15:17:09 +0000

    Remade user database, working on testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020140655) do
+ActiveRecord::Schema.define(version: 20171020151244) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
+  create_table "user_databases", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "email", null: false
     t.string "displayName"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
---
+++ spec/models/user_database_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe UserDatabase, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
