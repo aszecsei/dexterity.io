772549428a7e6e8a7c408e50d052586e6074b5c0
Jianhan Wang <jianhan-wang@uiowa.edu>
2017-10-22 20:19:31 +0000

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,35 @@
 class UsersController < ApplicationController
+    has_secure_password
+    has_secure_token
+    
     def new
     # default: render 'new' template
+    end
+  
+    def self.valid_login?(email, password)
+        user = find_by(email: email)
+        if user && user.authenticate(password)
+            user
+        end
+    end
+    
+    def allow_token_to_be_used_only_once
+        regenerate_token
+        touch(:token_created_at)
+    end
+
+    def logout
+        invalidate_token
+    end
+
+    def with_unexpired_token(token, period)
+        where(token: token).where('token_created_at >= ?', period).first
+    end
+    
+    private
+    
+    def invalidate_token
+        update_columns(token: nil)
+        touch(:token_created_at)
     end
 end
