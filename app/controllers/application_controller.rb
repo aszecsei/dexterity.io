ff7c6634fb783ff4a7532831cfa91597aad4d037
Alic Szecsei <aszecsei@gmail.com>
2017-11-03 17:19:44 +0000

    Fixed AJAX call for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  protected
+  
+  def require_login
+      authenticate_token || render_unauthorized("Access denied")
+  end
+
+  def current_user
+      @current_user ||= authenticate_token
+  end
+    
+  private
+  
+  def authenticate_token
+      authenticate_with_http_token do |token, options|
+          # Compare the tokens in a time-constant manner, to mitigate timing attacks
+          User.find_by(token: token)
+      end
+  end
 end
