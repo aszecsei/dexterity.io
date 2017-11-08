fb55ba51b39244ccebdb5f1647db3c346ca92b36
Alic Szecsei <aszecsei@gmail.com>
2017-11-08 18:20:07 +0000

    Refactor issues migration

--- db/migrate/20171108032805_create_issues.rb
+++ db/migrate/20171108032805_create_issues.rb
@@ -7,10 +7,7 @@ class CreateIssues < ActiveRecord::Migration[5.1]
       t.references :category, foreign_key: true, :null => false
       t.references :project, foreign_key: true, :null => false
       
-      t.references :assigned_to, foreign_key: { to_table: :users }
       t.references :created_by, foreign_key: { to_table: :users }, :null => false
-      
-      t.references :blocked_by, foreign_key: { to_table: :issues }
       
       t.string :estimated_time
       t.integer :story_points
