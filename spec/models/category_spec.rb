53f48b62571b041bbd7888030d18c5f1ec0e7436
Alic Szecsei <aszecsei@gmail.com>
2017-11-08 04:06:44 +0000

    Added tests for category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    proj = FactoryGirl.create(:project)
+    expect(FactoryGirl.build(:category, project: proj)).to be_valid
+  end
+  
+  it "is invalid without a name" do
+    proj = FactoryGirl.create(:project)
+    expect(FactoryGirl.build(:category, project: proj, name: nil)).to_not be_valid
+  end
+  
+  it "is invalid without a project" do
+    expect(FactoryGirl.build(:category, project: nil)).to_not be_valid
+  end
 end
