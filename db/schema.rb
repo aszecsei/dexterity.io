26c8ca5bbee688c7db59a66e622c9654305e3a0c
Alic Szecsei <aszecsei@gmail.com>
2017-11-08 03:18:39 +0000

    Add issue status table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103004412) do
+ActiveRecord::Schema.define(version: 20171108025819) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name", null: false
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(version: 20171103004412) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_roles_on_project_id"
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description"
+    t.string "category", default: "To Do", null: false
+    t.integer "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_statuses_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
---
+++ spec/factories/statuses.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :status do
+    name "MyString"
+    description "MyString"
+    category "MyString"
+    project nil
+  end
+end
