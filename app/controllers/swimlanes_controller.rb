f4cb767785fb7d3c4312b59af09f90ccdd8c9743
Tanner Brunscheon <tanner-brunscheon@uiowa.edu>
2017-11-09 20:22:43 +0000

    Alright first load works. Categories dont work but status does

--- app/controllers/swimlanes_controller.rb
+++ app/controllers/swimlanes_controller.rb
@@ -2,6 +2,12 @@ class SwimlanesController < ApplicationController
     before_action :require_login, raise: false
     
     def index
-        @title = "title"
+        id = params[:id] # retrieve project ID from URI route
+        @title = "Project #{id}"
+        @project = Project.find(id) # look up project by unique ID
+        @categories = @project.categories
+        #@swimlanes = @project.swimlanes
+        @user = @project.users
+
     end
 end
