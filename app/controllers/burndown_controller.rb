4977142f78d988c9ff9d5b8c3eafe25c00709d73
Alic Szecsei <aszecsei@gmail.com>
2017-11-30 09:05:49 +0000

    Dynamic charts

--- app/controllers/burndown_controller.rb
+++ app/controllers/burndown_controller.rb
@@ -1,3 +1,22 @@
 class BurndownController < ApplicationController
   before_action :require_login, raise: false
+  
+  def index
+    data = [
+      {
+        :date => '1/1/16',
+        :amt  => 12
+      },
+      {
+        :date => '1/2/16',
+        :amt  => 13
+      },
+      {
+        :date => '1/3/16',
+        :amt => 8
+      }
+      ]
+    @dates = "[" + data.map{|d| d[:date]}.inject(""){|sum,n| sum + "'" + n + "'" + ","} + "]"
+    @data = "[" + data.map{|d| d[:amt]}.inject(""){|sum,n| sum + n.to_s + ","} + "]"
+  end
 end
